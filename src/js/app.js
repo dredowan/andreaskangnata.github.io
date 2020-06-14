fc2971bd7cd985a937db83a157d8e15fe59abf6c
andreaskangnata <59155325+andreaskangnata@users.noreply.github.com>
2020-06-14 16:23:50 +0700

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,13 @@ var courseFeatureElements = document.querySelectorAll('.course-feature');
 var button = document.querySelector('button');
 
 navigator.serviceWorker.register('/sw.js');
+var deferredPrompt;
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
 
 function animate() {
   title.classList.remove('animate-in');
