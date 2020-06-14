41907004ad4aa67f982cbc275f559164719dea0f
andreaskangnata <59155325+andreaskangnata@users.noreply.github.com>
2020-06-14 16:46:07 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,8 +5,14 @@ self.addEventListener('install', function(event) {
         cache.addAll([
           '/',
           '/index.html',
+          '/manifest.json',
           '/src/css/app.css',
-          '/src/js/app.js'
+          '/src/js/app.js',
+          '/src/icons/icon-96x96.png',
+          '/src/icons/icon-144x144.png',
+          '/src/icons/icon-192x192.png',
+          '/src/icons/icon-384x384.png',
+          '/src/icons/icon-512x512.png'
         ])
       })
   );
